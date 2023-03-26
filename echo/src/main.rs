4492c67e1d15b05da2465a621057d7bd3945c7a1
Petri Avikainen <petri.avikainen@gmail.com>
2023-03-26 12:49:24 +0300

    Apparently println locks by default

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,12 +1,11 @@
 use serde_json::{json, Value};
-use std::io::Write;
 use std::io::{self, BufRead};
 
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 static MESSAGE_ID: AtomicUsize = AtomicUsize::new(0);
 
-fn respond(request: &Value, response: &Value) -> io::Result<()> {
+fn respond(request: &Value, response: &Value) -> () {
     let mut new_body = json!({
         "msg_id": MESSAGE_ID.fetch_add(1, Ordering::SeqCst),
         "in_reply_to": response["body"]["msg_id"]
@@ -17,32 +16,27 @@ fn respond(request: &Value, response: &Value) -> io::Result<()> {
         "dest": request["src"],
         "body": new_body
     });
-    let stdout = io::stdout();
-    let mut handle = stdout.lock();
-    print!("{}\n", msg);
-    return handle.flush();
+    println!("{}", msg)
 }
 
-fn respond_to_init_message(request: &Value) -> io::Result<()> {
+fn respond_to_init_message(request: &Value) -> () {
     let init_response = json!({
         "type": "init_ok",
         "in_reply_to": request["body"]["msg_id"],
     });
-    respond(request, &init_response)?;
-    Ok(())
+    respond(request, &init_response)
 }
 
-fn respond_to_echo_message(request: &Value) -> io::Result<()> {
+fn respond_to_echo_message(request: &Value) -> () {
     let echo_response = json!({
         "echo": request["body"]["echo"],
         "type": "echo_ok",
         "in_reply_to": request["body"]["msg_id"],
     });
-    respond(request, &echo_response)?;
-    Ok(())
+    respond(request, &echo_response)
 }
 
-fn handle_message(message: Value) -> io::Result<()> {
+fn handle_message(message: Value) -> () {
     match message["body"]["type"].as_str() {
         Some("init") => respond_to_init_message(&message),
         Some("echo") => respond_to_echo_message(&message),
@@ -53,7 +47,7 @@ fn handle_message(message: Value) -> io::Result<()> {
         Some(_) => {
             todo!()
         }
-        None => Ok(()),
+        None => (),
     }
 }
 
@@ -61,12 +55,13 @@ fn main() -> io::Result<()> {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         if let Some(message) = serde_json::from_str(line.unwrap().as_str())? {
-            handle_message(message)?
+            handle_message(message)
         }
     }
     Ok(())
 }
 
+// Copied from https://stackoverflow.com/questions/47070876/how-can-i-merge-two-json-objects-with-rust
 fn merge(a: &mut Value, b: Value) {
     if let Value::Object(a) = a {
         if let Value::Object(b) = b {
