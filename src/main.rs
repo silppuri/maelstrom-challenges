97968034ee1bb4d2c15b1b3259937f06ed6566a1
Petri Avikainen <petri.avikainen@gmail.com>
2023-03-26 18:04:27 +0300

    Generate unique id

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use maelstrom_challenges::utils::merge;
 use serde_json::{json, Value};
 use std::io::{self, BufRead};
 
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 static MESSAGE_ID: AtomicUsize = AtomicUsize::new(0);
+static RANDOM_ID: AtomicUsize = AtomicUsize::new(0);
 
 fn respond(request: &Value, response: &Value) -> () {
     let mut new_body = json!({
@@ -20,6 +22,7 @@ fn respond(request: &Value, response: &Value) -> () {
 }
 
 fn respond_to_init_message(request: &Value) -> () {
+    eprintln!("INIT MESSAGE: {}", request);
     let init_response = json!({
         "type": "init_ok",
         "in_reply_to": request["body"]["msg_id"],
@@ -36,6 +39,15 @@ fn respond_to_echo_message(request: &Value) -> () {
     respond(request, &echo_response)
 }
 
+fn respond_to_generate_message(request: &Value) -> () {
+    let generate_response = json!({
+        "type": "generate_ok",
+        "in_reply_to": request["body"]["msg_id"],
+        "id": format!("{}-{}", request["dest"].as_str().unwrap(), RANDOM_ID.fetch_add(1, Ordering::SeqCst)),
+    });
+    respond(request, &generate_response)
+}
+
 fn handle_message(message: Value) -> () {
     match message["body"]["type"].as_str() {
         Some("init") => respond_to_init_message(&message),
@@ -44,6 +56,7 @@ fn handle_message(message: Value) -> () {
             println!("ERROR: {}", message);
             panic!()
         }
+        Some("generate") => respond_to_generate_message(&message),
         Some(_) => {
             todo!()
         }
@@ -59,23 +72,4 @@ fn main() -> io::Result<()> {
         }
     }
     Ok(())
-}
-
-// Copied from https://stackoverflow.com/questions/47070876/how-can-i-merge-two-json-objects-with-rust
-fn merge(a: &mut Value, b: Value) {
-    if let Value::Object(a) = a {
-        if let Value::Object(b) = b {
-            for (k, v) in b {
-                if v.is_null() {
-                    a.remove(&k);
-                } else {
-                    merge(a.entry(k).or_insert(Value::Null), v);
-                }
-            }
-
-            return;
-        }
-    }
-
-    *a = b;
 }
